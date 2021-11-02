e95f4ad8b4bd868eabd99d890c569a2dd28655bd
李權恩 <85124403+Quan-En@users.noreply.github.com>
2021-11-03 04:27:20 +0800

    update

--- assignment2/gui_viewer.py
+++ assignment2/gui_viewer.py
@@ -42,6 +42,8 @@ class MainApplication(Tk, Transform, Conv_Filter, Hist_Enhencement):
         # variable
         self.image_array = ArrayVar()
         self.resize_method = IntVar()
+        self.sobel_direction = IntVar()
+        self.special_kernel = IntVar()
         
         # canvas
         self.canvas = Canvas(self, width=1024, height=1024)
@@ -74,18 +76,118 @@ class MainApplication(Tk, Transform, Conv_Filter, Hist_Enhencement):
         OprMenu.add_command(label="Resize", command=self.resize_opr)
         
         FilterMenu.add_command(label="Global Hist", command=self.global_hist_eq)
-        FilterMenu.add_command(label="Local Hist")
+        FilterMenu.add_command(label="Local Hist", command=self.local_hist_eq)
         FilterMenu.add_command(label="Hist match")
         FilterMenu.add_separator()
-        FilterMenu.add_command(label="Gaussian")
-        FilterMenu.add_command(label="Averaging")
+        FilterMenu.add_command(label="Gaussian", command=self.gaussian_blur)
+        FilterMenu.add_command(label="Averaging", command=self.average_blur)
         FilterMenu.add_separator()
-        FilterMenu.add_command(label="Unsharp")
+        FilterMenu.add_command(label="Unsharp", command=self.unsharp_edge_enhence)
         FilterMenu.add_separator()
-        FilterMenu.add_command(label="Laplacian")
-        FilterMenu.add_command(label="Sobel")
+        FilterMenu.add_command(label="Laplacian", command=self.laplacian_edge_detect)
+        FilterMenu.add_command(label="Sobel", command=self.sobel_edge_detect)
+        FilterMenu.add_separator()
+        FilterMenu.add_command(label="Median filter", command=self.median_denoise)
+        FilterMenu.add_command(label="Bilateral filter")
+        FilterMenu.add_command(label="Special filter", command=self.special_deal)
+        FilterMenu.add_separator()
+        FilterMenu.add_command(label="Non local means(NLM)")
         
         self.configure(menu=menubar)
+    
+    def global_hist_match(self):
+        pass
+    
+    def nl_means(self):
+        pass
+    
+    def bilateral_denoise(self):
+        pass
+    
+    def median_denoise(self):
+        new_wins = Toplevel(self)
+        new_wins.title("Special kernel")
+        new_wins.geometry("150x100")
+        
+        win_size_label = Label(new_wins, text ="kernel size: ")
+        win_size_label.grid(row=0, column=0)
+        
+        size_entry = Entry(new_wins, width=5)
+        size_entry.grid(row=0,column=1)
+        
+        def get_parm():
+            g_size = int(size_entry.get())
+            self.image_array.set(self.denoising(self.image_array.get(), g_size, 'median'))
+            self.new_img_canvas = ImageTk.PhotoImage(fromarray(self.image_array.get()))
+            self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
+            new_wins.destroy()
+        
+        get_btn = tk.Button(new_wins, text="Ok", command=get_parm)
+        get_btn.grid(row=3,column=0)
+    
+    def special_deal(self):
+        
+        new_wins = Toplevel(self)
+        new_wins.title("Special kernel")
+        new_wins.geometry("150x100")
+        
+        win_size_label = Label(new_wins, text ="kernel size: ")
+        win_size_label.grid(row=0, column=0)
+        
+        size_entry = Entry(new_wins, width=5)
+        size_entry.grid(row=0,column=1)
+        
+        rdio_1 = tk.Radiobutton(new_wins, text='kernel-1', variable=self.special_kernel, value=1)
+        rdio_2 = tk.Radiobutton(new_wins, text='kernel-2', variable=self.special_kernel, value=2)
+        
+        rdio_1.grid(row=2,column=0)
+        rdio_2.grid(row=3,column=0)
+        
+        def get_parm():
+            g_size = int(size_entry.get())
+            g_index = int(self.special_kernel.get())
+            self.image_array.set(self.special_filter(self.image_array.get(), g_size, 'gaussian', g_index))
+            self.new_img_canvas = ImageTk.PhotoImage(fromarray(self.image_array.get()))
+            self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
+            new_wins.destroy()
+        
+        get_btn = tk.Button(new_wins, text="Ok", command=get_parm)
+        get_btn.grid(row=4,column=0)
+    
+    def unsharp_edge_enhence(self):
+        new_wins = Toplevel(self)
+        new_wins.title("Unsharp edge enhence")
+        new_wins.geometry("180x150")
+        
+        win_size_label = Label(new_wins, text ="kernel size: ")
+        win_size_label.grid(row=0, column=0)
+        
+        size_entry = Entry(new_wins, width=5)
+        size_entry.grid(row=0,column=1)
+        
+        sigma_label = Label(new_wins, text ="sigma: ")
+        sigma_label.grid(row=1, column=0)
+        
+        sigma_entry = Entry(new_wins, width=5)
+        sigma_entry.grid(row=1,column=1)
+        
+        amount_label = Label(new_wins, text ="times amount: ")
+        amount_label.grid(row=2, column=0)
+        
+        amount_entry = Entry(new_wins, width=5)
+        amount_entry.grid(row=2,column=1)
+        
+        def get_parm():
+            g_size = int(size_entry.get())
+            g_sigma = float(sigma_entry.get())
+            g_amount = float(amount_entry.get())
+            self.image_array.set(self.unsharp(self.image_array.get(), g_size, 'gaussian', g_amount, sigma=g_sigma))
+            self.new_img_canvas = ImageTk.PhotoImage(fromarray(self.image_array.get()))
+            self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
+            new_wins.destroy()
+        
+        get_btn = tk.Button(new_wins, text="Ok", command=get_parm)
+        get_btn.grid(row=3,column=0)
     
     def global_hist_eq(self):
         self.image_array.set(self.HistEq(self.image_array.get()))
@@ -93,7 +195,123 @@ class MainApplication(Tk, Transform, Conv_Filter, Hist_Enhencement):
         self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
     
     def local_hist_eq(self):
-        pass
+        new_wins = Toplevel(self)
+        new_wins.title("Local Histogram Equalization")
+        new_wins.geometry("150x100")
+        
+        win_size_label = Label(new_wins, text ="kernel size: ")
+        win_size_label.grid(row=0, column=0)
+        
+        size_entry = Entry(new_wins, width=5)
+        size_entry.grid(row=1,column=0)
+        
+        def get_parm():
+            g_size = int(size_entry.get())
+            self.image_array.set(self.Local_HistEq(self.image_array.get(), g_size))
+            self.new_img_canvas = ImageTk.PhotoImage(fromarray(self.image_array.get()))
+            self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
+            new_wins.destroy()
+        
+        get_btn = tk.Button(new_wins, text="Ok", command=get_parm)
+        get_btn.grid(row=2,column=0)
+    
+    def gaussian_blur(self):
+        new_wins = Toplevel(self)
+        new_wins.title("Gaussian blur")
+        new_wins.geometry("150x100")
+        
+        win_size_label = Label(new_wins, text ="kernel size: ")
+        win_size_label.grid(row=0, column=0)
+        
+        size_entry = Entry(new_wins, width=5)
+        size_entry.grid(row=0,column=1)
+        
+        sigma_label = Label(new_wins, text ="sigma: ")
+        sigma_label.grid(row=1, column=0)
+        
+        sigma_entry = Entry(new_wins, width=5)
+        sigma_entry.grid(row=1,column=1)
+        
+        def get_parm():
+            g_size = int(size_entry.get())
+            g_sigma = float(sigma_entry.get())
+            self.image_array.set(self.blur(self.image_array.get(), g_size, 'gaussian', sigma=g_sigma))
+            self.new_img_canvas = ImageTk.PhotoImage(fromarray(self.image_array.get()))
+            self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
+            new_wins.destroy()
+        
+        get_btn = tk.Button(new_wins, text="Ok", command=get_parm)
+        get_btn.grid(row=2,column=0)
+        
+    def average_blur(self):
+        new_wins = Toplevel(self)
+        new_wins.title("Averaging blur")
+        new_wins.geometry("150x100")
+        
+        win_size_label = Label(new_wins, text ="kernel size: ")
+        win_size_label.grid(row=0, column=0)
+        
+        size_entry = Entry(new_wins, width=5)
+        size_entry.grid(row=1,column=0)
+        
+        def get_parm():
+            g_size = int(size_entry.get())
+            self.image_array.set(self.blur(self.image_array.get(), g_size, 'average'))
+            self.new_img_canvas = ImageTk.PhotoImage(fromarray(self.image_array.get()))
+            self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
+            new_wins.destroy()
+        
+        get_btn = tk.Button(new_wins, text="Ok", command=get_parm)
+        get_btn.grid(row=2,column=0)
+    
+    def laplacian_edge_detect(self):
+        new_wins = Toplevel(self)
+        new_wins.title("Laplacian edge detect")
+        new_wins.geometry("150x100")
+        
+        win_size_label = Label(new_wins, text ="kernel size: ")
+        win_size_label.grid(row=0, column=0)
+        
+        size_entry = Entry(new_wins, width=5)
+        size_entry.grid(row=1,column=0)
+        
+        def get_parm():
+            g_size = int(size_entry.get())
+            self.image_array.set(self.laplacian_filter(self.image_array.get(), g_size))
+            self.new_img_canvas = ImageTk.PhotoImage(fromarray(self.image_array.get()))
+            self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
+            new_wins.destroy()
+        
+        get_btn = tk.Button(new_wins, text="Ok", command=get_parm)
+        get_btn.grid(row=2,column=0)
+        
+    def sobel_edge_detect(self):
+        new_wins = Toplevel(self)
+        new_wins.title("Sobel edge detect")
+        new_wins.geometry("180x150")
+        
+        win_size_label = Label(new_wins, text ="kernel size: ")
+        win_size_label.grid(row=0, column=0)
+        
+        size_entry = Entry(new_wins, width=5)
+        size_entry.grid(row=0,column=1)
+        
+        rdio_row = tk.Radiobutton(new_wins, text='By row', variable=self.sobel_direction, value=1)
+        rdio_col = tk.Radiobutton(new_wins, text='By column', variable=self.sobel_direction, value=2)
+        
+        rdio_row.grid(row=2,column=0)
+        rdio_col.grid(row=3,column=0)
+        
+        def get_parm():
+            g_size = int(size_entry.get())
+            g_dir = int(self.sobel_direction.get())
+            self.image_array.set(self.sobel_filter(self.image_array.get(), g_size, g_dir))
+            self.new_img_canvas = ImageTk.PhotoImage(fromarray(self.image_array.get()))
+            self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
+            new_wins.destroy()
+        
+        get_btn = tk.Button(new_wins, text="Ok", command=get_parm)
+        get_btn.grid(row=4,column=0)
         
     def open_image(self):
         # global new_img_canvas
@@ -120,25 +338,25 @@ class MainApplication(Tk, Transform, Conv_Filter, Hist_Enhencement):
         self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
     
     def gamma_opr(self):
-        gamma_trans_new_wins = Toplevel(self)
-        gamma_trans_new_wins.title("gamma transform")
-        gamma_trans_new_wins.geometry("150x100")
+        new_wins = Toplevel(self)
+        new_wins.title("gamma transform")
+        new_wins.geometry("150x100")
         
-        power_label = Label(gamma_trans_new_wins, text ="Power: ")
+        power_label = Label(new_wins, text ="Power: ")
         power_label.grid(row=0, column=0)
         
-        power_entry = Entry(gamma_trans_new_wins, width=5)
+        power_entry = Entry(new_wins, width=5)
         power_entry.grid(row=1,column=0)
         
-        def get_power():
+        def get_parm():
             g_power = float(power_entry.get())
             self.image_array.set(self.gamma_trans(self.image_array.get(), g_power))
             self.new_img_canvas = ImageTk.PhotoImage(fromarray(self.image_array.get()))
             self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
-            gamma_trans_new_wins.destroy()
+            new_wins.destroy()
         
-        get_power_btn = tk.Button(gamma_trans_new_wins, text="Ok", command=get_power)
-        get_power_btn.grid(row=2,column=0)
+        get_btn = tk.Button(new_wins, text="Ok", command=get_parm)
+        get_btn.grid(row=2,column=0)
     
     def neg_opr(self):
         self.image_array.set(self.neg_trans(self.image_array.get()))
@@ -146,43 +364,43 @@ class MainApplication(Tk, Transform, Conv_Filter, Hist_Enhencement):
         self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
         
     def resize_opr(self):
-        resize_trans_new_wins = Toplevel(self)
-        resize_trans_new_wins.title("Resize")
-        resize_trans_new_wins.geometry("200x180")
+        new_wins = Toplevel(self)
+        new_wins.title("Resize")
+        new_wins.geometry("200x180")
         
-        height_label = Label(resize_trans_new_wins, text ="Height: ")
+        height_label = Label(new_wins, text ="Height: ")
         height_label.grid(row=1, column=0)
         
-        height_entry = Entry(resize_trans_new_wins, width=5)
+        height_entry = Entry(new_wins, width=5)
         height_entry.grid(row=1,column=1)
         
-        width_label = Label(resize_trans_new_wins, text ="Width: ")
+        width_label = Label(new_wins, text ="Width: ")
         width_label.grid(row=2, column=0)
         
-        width_entry = Entry(resize_trans_new_wins, width=5)
+        width_entry = Entry(new_wins, width=5)
         width_entry.grid(row=2,column=1)
         
-        rdio_bilinear = tk.Radiobutton(resize_trans_new_wins, text='Bilinear', variable=self.resize_method, value=1)
-        rdio_neighbor = tk.Radiobutton(resize_trans_new_wins, text='Neighbor', variable=self.resize_method, value=2)
+        rdio_bilinear = tk.Radiobutton(new_wins, text='Bilinear', variable=self.resize_method, value=1)
+        rdio_neighbor = tk.Radiobutton(new_wins, text='Neighbor', variable=self.resize_method, value=2)
         
         rdio_bilinear.grid(row=3,column=0)
         rdio_neighbor.grid(row=4,column=0)
         
-        def get_height_and_width():
+        def get_parm():
             
             new_height = int(height_entry.get())
             new_width = int(width_entry.get())
-            
-            new_resize_img = self.image_resize(self.image_array.get(), new_height, new_width, mode=int(self.resize_method.get()))
+            new_mode = int(self.resize_method.get())
+            new_resize_img = self.image_resize(self.image_array.get(), new_height, new_width, mode=new_mode)
             self.image_array.set(new_resize_img)
             
             self.new_img_canvas = ImageTk.PhotoImage(fromarray(self.image_array.get()))
             self.canvas.itemconfigure(self.image_on_canvas, image=self.new_img_canvas)
             
-            resize_trans_new_wins.destroy()
+            new_wins.destroy()
         
-        get_h_and_w_btn = tk.Button(resize_trans_new_wins, text="Ok", command=get_height_and_width)
-        get_h_and_w_btn.grid(row=5,column=1)
+        get_btn = tk.Button(new_wins, text="Ok", command=get_parm)
+        get_btn.grid(row=5,column=1)
             
 app=MainApplication()
 app.mainloop()
